0188260ae865d5159f069555d6841314bbb980a5
SHIVAM <shivam@SHIVAMs-MacBook.local>
2026-03-13 17:00:28 +0530

    Streams EMS RealTimeQueries

---
+++ JavaBasicsPractice/src/main/java/Streams/ASD.java
@@ -0,0 +1,11 @@
+package Streams;
+
+public class ASD {
+    public static void main(String[] args) {
+            String str = "Hello World";
+            String[] arr = str.split(" ");
+            for (String s : arr) {
+                System.out.println(s);
+            }
+    }
+}
