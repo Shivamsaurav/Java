c6e8957301143bf5a281efcada00f0d6cfe0afef
SHIVAM <shivam@SHIVAMs-MacBook.local>
2026-04-22 06:35:53 +0530

    Fail Fast & Fail Safe Iterator

---
+++ JavaBasicsPractice/src/main/java/miscellaneous/Q03_FailFastExample.java
@@ -0,0 +1,28 @@
+package miscellaneous;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Q03_FailFastExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("New Delhi", "India");
+		map.put("Moscow", "Russia");
+		map.put("New York", "USA");
+		
+		Iterator iterator = map.keySet().iterator();
+		
+		while (iterator.hasNext()) {
+			System.out.println(map.get(iterator.next()));
+			
+			map.put("Istanbul", "Turkey");
+			
+		}
+	}
+
+}
---
+++ JavaBasicsPractice/src/main/java/miscellaneous/Q04_FailSafeExample.java
@@ -0,0 +1,28 @@
+package miscellaneous;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Q04_FailSafeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String, String>();
+		
+		map.put("New Delhi", "India");
+		map.put("Moscow", "Russia");
+		map.put("New York", "USA");
+		
+		Iterator iterator = map.keySet().iterator();
+		
+		while (iterator.hasNext()) {
+			System.out.println(map.get(iterator.next()));
+			
+			map.put("Istanbul", "Turkey");
+			
+		}
+
+	}
+
+}
